46e0d81f94879ac940d9bf2d3196a37f4f2ab1a7
yingfm <yingfangming@163.com>
2017-06-13 16:09:25 +0800

    add list

--- xuexi02.py
+++ xuexi02.py
@@ -4,7 +4,8 @@ dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'};
 
 dict['Age'] = 8;  # update existing entry
 dict['School'] = "DPS School";  # Add new entry
-dict['Name'] = 'yfm03';
+dict['Name'] = 'yfm04';
+list = [1,2,3,4]
 
 print "dict['Age']: ", dict['Age'];
 print "dict['School']: ", dict['School'];
