10b613d9b49bd5d8b47849d34c1bbf857d3aadb5
yingfm <yingfangming@163.com>
2017-06-13 15:37:12 +0800

    new xuexi02.py

--- xuexi02.py
+++ xuexi02.py
@@ -4,7 +4,8 @@ dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'};
 
 dict['Age'] = 8;  # update existing entry
 dict['School'] = "DPS School";  # Add new entry
-dict['Name'] = 'yfm';
+dict['Name'] = 'yfm02';
 
 print "dict['Age']: ", dict['Age'];
 print "dict['School']: ", dict['School'];
+print "dict['Name']: ", dict['Name'];
